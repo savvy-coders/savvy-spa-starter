6903581551dbec8e920438d735d1574f72b71fb8
Cameron Brantley <brantley.cameron@gmail.com>
2021-05-17 21:25:58 -0500

    Refactor to original

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-import html from "html-literal";
-
-export default () => html`
-  <footer>
-    &copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
-  </footer>
-`;
--- components/views/Home.js
+++
@@ -1,8 +0,0 @@
-import html from "html-literal";
-
-export default () => html`
-  <section id="jumbotron">
-    <h2>Savvy Coders Apr. 2021 Cohort</h2>
-    <a href="register.html">"Call to Action" "Button"</a>
-  </section>
-`;
